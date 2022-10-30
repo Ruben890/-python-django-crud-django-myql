88575a26672fdfa678ac50d6b927bd4331f47f26
Darlin Ruben Nina C <78616515+Ruben890@users.noreply.github.com>
2022-10-30 15:03:00 -0400

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -6,10 +6,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
